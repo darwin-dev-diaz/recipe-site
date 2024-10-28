c362cb7894ef4d32987f35bbf28291647e85659a
Darwin Diaz <152736334+darwin-dev-diaz@users.noreply.github.com>
2024-10-28 10:31:02 -0400

    Add new svgs

---
+++ src/assets/icons/ArrowLeft.jsx
@@ -0,0 +1,3 @@
+import * as React from "react";
+const SvgArrowLeft = props => <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} fill="#5f6368" viewBox="0 -960 960 960" {...props}><path d="M560-280 360-480l200-200z" /></svg>;
+export default SvgArrowLeft;
---
+++ src/assets/icons/ArrowRight.jsx
@@ -0,0 +1,3 @@
+import * as React from "react";
+const SvgArrowRight = props => <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} fill="#5f6368" viewBox="0 -960 960 960" {...props}><path d="M400-280v-400l200 200z" /></svg>;
+export default SvgArrowRight;
