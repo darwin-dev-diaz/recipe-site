4c407442ccf4039d76ad8e0e438e80da79abd615
Darwin Diaz <152736334+darwin-dev-diaz@users.noreply.github.com>
2024-11-19 11:32:05 -0500

    Be able to select prev month

--- src/pages/MonthlyPlanScreen.jsx
+++ src/pages/MonthlyPlanScreen.jsx
@@ -20,6 +20,7 @@ function MonthlyPlanScreen() {
     "November",
     "December",
   ];
+
   const todaysYear = today.getFullYear();
   const todaysMonth = today.getMonth() + 1;
   const todaysDay = today.getDate();
@@ -28,18 +29,18 @@ function MonthlyPlanScreen() {
     day: todaysDay,
     year: todaysYear,
   });
-
   const getMonthInfo = (y, m) => {
     const numDays = new Date(y, m, 0).getDate();
-    const startDate = new Date(y, m, 0).getDay();
+    const startDate = new Date(y, m - 1).getDay() + 1;
+    console.log(new Date(y, m - 1));
+    console.log({ startDate });
     return { numDays, startDate };
   };
 
-  const startDay = getMonthInfo(
-    selectedDate.year,
-    selectedDate.month,
-  ).startDate;
-
+  // console.log({
+  //   selectedDate,
+  //   startDay: getMonthInfo(selectedDate.year, selectedDate.month).startDate,
+  // });
   const styles = {
     selectedStylesItem: {
       borderRadius: "0.5rem",
@@ -48,15 +49,31 @@ function MonthlyPlanScreen() {
     },
     selectedStylesText: { color: "white" },
     selectedStylesDot: { backgroundColor: "white", color: "white" },
+    startDayStyle: {
+      gridColumnStart: getMonthInfo(selectedDate.year, selectedDate.month)
+        .startDate,
+    },
   };
 
   return (
     <>
       <div className="mb-8 mt-4 flex items-center justify-center">
-        <SvgArrowLeft className="h-10 w-10"></SvgArrowLeft>
+        <SvgArrowLeft
+          onClick={() => {
+            const date = new Date(selectedDate.year, selectedDate.month - 1);
+            const lastMonth = new Date(date.getFullYear(), date.getMonth() - 1);
+            lastMonth.setDate(1);
+            setSelectedDate({
+              month: lastMonth.getMonth() + 1,
+              day: 1,
+              year: lastMonth.getFullYear(),
+            });
+          }}
+          className="h-10 w-10"
+        ></SvgArrowLeft>
         <div className="z-40 mx-auto flex h-12 w-full max-w-96 items-center bg-orange">
           <h2 className="text-stroke-black text-stroke-2 stroke-text smooth-16 relative w-fit pl-3 text-3xl font-extrabold uppercase text-white">
-            {months[todaysMonth - 1]}
+            {`${months[selectedDate.month - 1]}, ${selectedDate.year}`}
           </h2>
         </div>
         <SvgArrowRight className="h-10 w-10"></SvgArrowRight>
@@ -75,41 +92,86 @@ function MonthlyPlanScreen() {
           {Array.from(
             { length: getMonthInfo(todaysYear, todaysMonth).numDays },
             (_, i) => i + 1,
-          ).map((i) => (
-            <div
-              onClick={() => {
-                setSelectedDate((prev) => ({ ...prev, day: i }));
-              }}
-              key={i}
-              className="flex h-12 w-10 cursor-pointer flex-col items-center justify-center"
-              style={
-                ({
-                  gridColumnStart: `${i === 1 ? startDay : ""}`,
-                },
-                i === selectedDate.day ? styles.selectedStylesItem : null)
-              }
-            >
-              <span
-                className="text-3xl font-bold"
-                style={
-                  i === selectedDate.day ? styles.selectedStylesText : null
-                }
-              >
-                {i}
-              </span>
-              <div className="flex h-[10px] gap-1">
-                {Array.from(Array(i % 3).keys()).map((x) => (
+          ).map((i) => {
+            let style;
+            switch (true) {
+              case i === 1 && selectedDate.day === 1:
+                style = { ...styles.selectedStylesItem };
+                return (
                   <div
-                    key={x}
-                    className="h-[10px] w-[10px] rounded-full bg-black"
-                    style={
-                      i === selectedDate.day ? styles.selectedStylesDot : null
-                    }
-                  ></div>
-                ))}
+                    onClick={() => {
+                      setSelectedDate((prev) => ({ ...prev, day: i }));
+                    }}
+                    key={i}
+                    className="here flex h-12 w-10 cursor-pointer flex-col items-center justify-center"
+                    style={{
+                      ...style,
+                      gridColumnStart:
+                        getMonthInfo(selectedDate.year, selectedDate.month)
+                          .startDate ,
+                    }}
+                  >
+                    <span
+                      className="text-3xl font-bold"
+                      style={styles.selectedStylesText}
+                    >
+                      {i}
+                    </span>
+                    <div className="flex h-[10px] gap-1">
+                      {Array.from(Array(i % 3).keys()).map((x) => (
+                        <div
+                          key={x}
+                          className="h-[10px] w-[10px] rounded-full bg-black"
+                          style={
+                            i === selectedDate.day
+                              ? styles.selectedStylesDot
+                              : null
+                          }
+                        ></div>
+                      ))}
+                    </div>
+                  </div>
+                );
+              case i === 1:
+                style = styles.startDayStyle;
+                break;
+              case i === selectedDate.day:
+                style = styles.selectedStylesItem;
+                break;
+              default:
+                style = {};
+            }
+            return (
+              <div
+                onClick={() => {
+                  setSelectedDate((prev) => ({ ...prev, day: i }));
+                }}
+                key={i}
+                className="here flex h-12 w-10 cursor-pointer flex-col items-center justify-center"
+                style={style}
+              >
+                <span
+                  className="text-3xl font-bold"
+                  style={
+                    i === selectedDate.day ? styles.selectedStylesText : null
+                  }
+                >
+                  {i}
+                </span>
+                <div className="flex h-[10px] gap-1">
+                  {Array.from(Array(i % 3).keys()).map((x) => (
+                    <div
+                      key={x}
+                      className="h-[10px] w-[10px] rounded-full bg-black"
+                      style={
+                        i === selectedDate.day ? styles.selectedStylesDot : null
+                      }
+                    ></div>
+                  ))}
+                </div>
               </div>
-            </div>
-          ))}
+            );
+          })}
         </div>
       </div>
 
