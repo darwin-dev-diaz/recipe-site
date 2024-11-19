6f6e1be43ed23712e71dc19d1b6c35f7a3de27cd
Darwin Diaz <152736334+darwin-dev-diaz@users.noreply.github.com>
2024-11-19 09:40:52 -0500

    Create selectedDate state

--- src/pages/MonthlyPlanScreen.jsx
+++ src/pages/MonthlyPlanScreen.jsx
@@ -2,10 +2,45 @@ import SvgArrowLeft from "../assets/icons/ArrowLeft";
 import SvgArrowRight from "../assets/icons/ArrowRight";
 import RemoveableRecipe from "../components/RemoveableRecipe";
 import image from "../assets/images/pancake.jpg";
+import { useState } from "react";
 
 function MonthlyPlanScreen() {
-  const startDay = 7;
-  const selected = 30;
+  const today = new Date();
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  const todaysYear = today.getFullYear();
+  const todaysMonth = today.getMonth() + 1;
+  const todaysDay = today.getDate();
+
+  const [selectedDate, setSelectedDate] = useState({
+    month: todaysMonth,
+    day: todaysDay,
+    year: todaysYear,
+  });
+
+  const getMonthInfo = (y, m) => {
+    const numDays = new Date(y, m, 0).getDate();
+    const startDate = new Date(y, m, 0).getDay();
+    return { numDays, startDate };
+  };
+
+  const startDay = getMonthInfo(
+    selectedDate.year,
+    selectedDate.month,
+  ).startDate;
+
   const styles = {
     selectedStylesItem: {
       borderRadius: "0.5rem",
@@ -22,7 +57,7 @@ function MonthlyPlanScreen() {
         <SvgArrowLeft className="h-10 w-10"></SvgArrowLeft>
         <div className="z-40 mx-auto flex h-12 w-full max-w-96 items-center bg-orange">
           <h2 className="text-stroke-black text-stroke-2 stroke-text smooth-16 relative w-fit pl-3 text-3xl font-extrabold uppercase text-white">
-            October&apos;s recipes
+            {months[todaysMonth - 1]}
           </h2>
         </div>
         <SvgArrowRight className="h-10 w-10"></SvgArrowRight>
@@ -38,7 +73,10 @@ function MonthlyPlanScreen() {
           <span className="row-span-1 mb-4 text-xl font-bold">S</span>
         </div>
         <div className="grid h-auto grid-cols-7 grid-rows-5 items-center justify-items-center gap-y-8 bg-very-light-grey px-7 py-4">
-          {Array.from({ length: 30 }, (_, i) => i + 1).map((i) => (
+          {Array.from(
+            { length: getMonthInfo(todaysYear, todaysMonth).numDays },
+            (_, i) => i + 1,
+          ).map((i) => (
             <div
               key={i}
               className="flex h-12 w-10 flex-col items-center justify-center"
@@ -46,12 +84,14 @@ function MonthlyPlanScreen() {
                 ({
                   gridColumnStart: `${i === 1 ? startDay : ""}`,
                 },
-                i === selected ? styles.selectedStylesItem : null)
+                i === selectedDate.day ? styles.selectedStylesItem : null)
               }
             >
               <span
                 className="text-3xl font-bold"
-                style={i === selected ? styles.selectedStylesText : null}
+                style={
+                  i === selectedDate.day ? styles.selectedStylesText : null
+                }
               >
                 {i}
               </span>
@@ -60,7 +100,9 @@ function MonthlyPlanScreen() {
                   <div
                     key={x}
                     className="h-[10px] w-[10px] rounded-full bg-black"
-                    style={i === selected ? styles.selectedStylesDot : null}
+                    style={
+                      i === selectedDate.day ? styles.selectedStylesDot : null
+                    }
                   ></div>
                 ))}
               </div>
@@ -69,11 +111,12 @@ function MonthlyPlanScreen() {
         </div>
       </div>
 
-      <h3 className="px-6 text-center text-3xl font-bold uppercase mb-8">
-        Month {selected} meal plan
+      <h3 className="mb-8 px-6 text-center text-3xl font-bold uppercase">
+        {`${months[selectedDate.month - 1]} ${selectedDate.day}, ${selectedDate.year}`}{" "}
+        Meal Plan
       </h3>
 
-      <div className="flex w-full gap-4 overflow-x-auto px-2 mb-14">
+      <div className="mb-14 flex w-full gap-4 overflow-x-auto px-2">
         <div>
           <p className="mb-2 text-xl font-bold uppercase">Breakfast</p>
           <RemoveableRecipe image={image}></RemoveableRecipe>
