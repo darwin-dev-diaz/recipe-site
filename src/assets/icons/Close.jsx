1855484db19f79447cf02b482a51fd1719cfa19e
Darwin Diaz <152736334+darwin-dev-diaz@users.noreply.github.com>
2024-11-20 11:12:53 -0500

    Add svgClose

---
+++ src/assets/icons/Close.jsx
@@ -0,0 +1,17 @@
+const SvgClose = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={24}
+    height={24}
+    fill="black"
+    viewBox="0 -960 960 960"
+    {...props}
+  >
+    <path
+      d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224z"
+      fill="black"
+      stroke="black"
+    />
+  </svg>
+);
+export default SvgClose;
