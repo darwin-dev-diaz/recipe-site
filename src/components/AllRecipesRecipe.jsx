2720854c7d396cc9244ffa9ce1005950983f2736
Darwin Diaz <152736334+darwin-dev-diaz@users.noreply.github.com>
2024-11-22 10:22:01 -0500

    Make allRecipes screen responsive

--- src/components/AllRecipesRecipe.jsx
+++ src/components/AllRecipesRecipe.jsx
@@ -4,20 +4,20 @@ import { Link } from "react-router-dom";
 function AllRecipesRecipe(props) {
   return (
     <div
-      className="flex max-h-[28rem] h-[28rem] w-[13.5rem] flex-col justify-between place-self-center justify-self-center"
+      className="flex h-[28rem] max-h-[28rem] w-[13.5rem] flex-col justify-between place-self-center justify-self-center sm:h-[24rem] sm:w-[80%] md:h-[26rem]"
       style={props.cssObj ? props.cssObj : {}}
     >
       <div
-        className="h-[21rem] w-full self-center bg-black bg-cover bg-center"
+        className="h-[21rem] w-full self-center bg-black bg-cover bg-center sm:h-[18rem] md:h-[20rem]"
         style={{ backgroundImage: `url(${props.image})` }}
       ></div>
-      <span className="my-3 text-center text-xl font-extrabold uppercase">
+      <span className="my-3 text-center text-base font-extrabold uppercase sm:text-lg md:text-xl">
         {props.title}
       </span>
       <Link to={props.to ? props.to : null}>
         <Button
           width="w-full"
-          height="h-[50px]"
+          height="h-[40px] sm:h-[45px] md:h-[50px]"
           color={props.buttonColor ? props.buttonColor : "orange"}
           textColor="white"
           text={props.buttonText ? props.buttonText : "view recipe"}
--- src/pages/AllRecipesScreen.jsx
+++ src/pages/AllRecipesScreen.jsx
@@ -3,11 +3,13 @@ import { useContext, useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { idToImage } from "../util/idToImage";
 import { useState } from "react";
+
 import Loading from "../components/primatives/Loading";
 import Error from "../components/primatives/Error";
 import Button from "../components/primatives/Button";
-import SvgFilterList from "../assets/icons/FilterList";
 import AllRecipesRecipe from "../components/AllRecipesRecipe";
+
+import SvgFilterList from "../assets/icons/FilterList";
 
 function AllRecipesScreen() {
   const { data, loading, error } = useContext(RecipeContext);
@@ -45,56 +47,58 @@ function AllRecipesScreen() {
   if (error) return <Error />;
   if (loading) return <Loading />;
   return (
-    <div className="px-6">
-      <div className="z-40 mx-auto mb-6 mt-4 flex h-12 w-full max-w-96 items-center bg-orange">
-        <h2 className="text-stroke-black text-stroke-2 stroke-text smooth-16 relative w-fit pl-3 text-3xl font-extrabold uppercase text-white">
-          All Recipes
-        </h2>
-      </div>
-      <div className="mx-auto mb-6 flex w-[11rem] cursor-pointer flex-col items-center">
-        <Button
-          height="h-[50px]"
-          color="very-light-grey"
-          textColor="dark-grey"
-          text="Sort by..."
-          extraCss="mx-auto"
-          onClick={() => setSortOpen(!sortOpen)}
-        >
-          <SvgFilterList color=""></SvgFilterList>
-        </Button>
+    <div className="flex w-full items-center justify-center px-6">
+      <div className="max-w-[72rem]">
+        <div className="z-40 mx-auto mb-6 mt-4 flex h-12 w-full max-w-96 items-center bg-orange">
+          <h2 className="text-stroke-black text-stroke-2 stroke-text smooth-16 relative w-fit pl-3 text-2xl font-extrabold uppercase text-white md:text-3xl">
+            All Recipes
+          </h2>
+        </div>
+        <div className="mx-auto mb-6 flex w-full max-w-xs cursor-pointer flex-col items-center md:max-w-sm">
+          <Button
+            height="h-[50px]"
+            color="very-light-grey"
+            textColor="dark-grey"
+            text="Sort by..."
+            extraCss="mx-auto"
+            onClick={() => setSortOpen(!sortOpen)}
+          >
+            <SvgFilterList color=""></SvgFilterList>
+          </Button>
 
-        {sortOpen ? (
-          <div className="flex h-fit w-[11rem] flex-col justify-center gap-4 bg-very-light-grey p-4">
-            {dishTypes.map((item, i) => (
-              <div
-                key={i}
-                className="justify-left flex items-center gap-2"
-                onClick={() => setSelected(i)}
-              >
+          {sortOpen ? (
+            <div className="flex h-fit w-full flex-col justify-center gap-4 bg-very-light-grey p-4">
+              {dishTypes.map((item, i) => (
                 <div
-                  className={`h-[15px] w-[15px] rounded-full`}
-                  style={{
-                    backgroundColor: `${selected === -1 ? "lightgrey" : selected === i ? "var(--black)" : "lightgrey"}`,
-                  }}
-                ></div>
-                <div className="text-sm font-bold uppercase text-black">
-                  {item}
+                  key={i}
+                  className="flex items-center gap-2"
+                  onClick={() => setSelected(i)}
+                >
+                  <div
+                    className={`h-[15px] w-[15px] rounded-full`}
+                    style={{
+                      backgroundColor: `${selected === -1 ? "lightgrey" : selected === i ? "var(--black)" : "lightgrey"}`,
+                    }}
+                  ></div>
+                  <div className="text-sm font-bold uppercase text-black">
+                    {item}
+                  </div>
                 </div>
-              </div>
-            ))}
-          </div>
-        ) : null}
-      </div>
+              ))}
+            </div>
+          ) : null}
+        </div>
 
-      <div className="section mb-16 grid w-full grid-cols-2 items-center justify-center gap-y-3">
-        {selectedData.map((recipe, i) => (
-          <AllRecipesRecipe
-            key={i}
-            image={idToImage(recipe.id)}
-            title={recipe.title}
-            to={`/recipe/${recipe.id}`}
-          ></AllRecipesRecipe>
-        ))}
+        <div className="section mb-16 grid grid-cols-2 items-center justify-center gap-x-4 gap-y-3 justify-self-center sm:gap-x-0 md:grid-cols-3 md:gap-y-20 lg:w-[70%]">
+          {selectedData.map((recipe, i) => (
+            <AllRecipesRecipe
+              key={i}
+              image={idToImage(recipe.id)}
+              title={recipe.title}
+              to={`/recipe/${recipe.id}`}
+            ></AllRecipesRecipe>
+          ))}
+        </div>
       </div>
     </div>
   );
