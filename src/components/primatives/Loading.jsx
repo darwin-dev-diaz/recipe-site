f1e6ca7fd48092b933ec2f62f9b53ea839d46816
Darwin Diaz <152736334+darwin-dev-diaz@users.noreply.github.com>
2024-11-25 12:09:45 -0500

    update loading screen

--- src/components/primatives/Loading.jsx
+++ src/components/primatives/Loading.jsx
@@ -4,6 +4,9 @@ function Loading() {
   return (
     <div className="h-[75vh] w-full flex justify-center items-center">
       <img className="mx-auto w-24" src={loadingGif} />
+      <h2 className="mb-6 text-lg font-bold uppercase">
+            Fetching API, this may take a bit...
+          </h2>
     </div>
   );
 }
